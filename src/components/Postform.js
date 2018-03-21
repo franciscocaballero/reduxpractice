d052d334125a8fb8d7acf0463c3fbfee523c8d39
franciscocaballero <caballerofrancisco22@yahoo.com>
2018-03-20 23:48:05 -0400

    added very simple form

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 
 
 class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
   render() {
     return (
       <div>
-      MY COMPONENT<
-      /div>
+        <h1>Add Post</h1>
+        <form>
+          <div>
+          <label>Title: </label>
+          <br />
+          <input type ='text' name='title'/>
+          </div>
+          <div>
+          <label>Body: </label>
+          <br />
+          <textarea name='body'/>
+          </div>
+          <br />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
     );
   }
 }
