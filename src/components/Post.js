fba3a09590c2e780c9bb12cbbc6bb4b1ddcb4bfc
franciscocaballero <caballerofrancisco22@yahoo.com>
2018-03-20 23:17:19 -0400

    first commit

--- src/components/Post.js
+++ src/components/Post.js
@@ -13,13 +13,21 @@ class Post extends Component {
   componentWillMount(){
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => this.setState({posts: data}));
   }
-  
+
   render() {
+
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ))
     return (
       <div>
       <h1>POST</h1>
+      { postItems}
       </div>
     );
   }
